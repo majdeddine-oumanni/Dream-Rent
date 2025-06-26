32df629b971eaf22fe46f9ccbbc2c0467e515874
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-06-26 03:03:21 +0100

    creating CRUD functions for the Properties in service layer

--- backend/src/main/java/com/rent/backend/Service/PropertyService.java
+++ backend/src/main/java/com/rent/backend/Service/PropertyService.java
@@ -1,8 +1,12 @@
 package com.rent.backend.Service;
 
+import com.rent.backend.DTO.PropertyDTO;
 import com.rent.backend.Mappers.PropertyMapper;
+import com.rent.backend.Model.Property;
 import com.rent.backend.Repositories.PropertyRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PropertyService {
@@ -14,5 +18,33 @@ public class PropertyService {
         this.mapper = mapper;
     }
 
-    
+    public PropertyDTO create(PropertyDTO dto){
+        Property property = mapper.toEntity(dto);
+        return mapper.toDTO(repository.save(property));
+    }
+
+    public PropertyDTO update(Long id, PropertyDTO dto){
+        Property property = repository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Property not found"));
+
+        property.setAvailability(dto.isAvailability());
+        property.setLocation(dto.getLocation());
+        property.setDescription(dto.getDescription());
+        property.setType(dto.getType());
+        property.setRoomsNumber(dto.getRoomsNumber());
+
+        Property savedProperty = repository.save(property);
+
+        return mapper.toDTO(savedProperty);
+    }
+
+    public List<PropertyDTO> getAllProperties(){
+        List<Property> properties = repository.findAll();
+        return mapper.toDTOs(properties);
+    }
+
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
 }
