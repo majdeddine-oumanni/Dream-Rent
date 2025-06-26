d72099cb6ba061dcdb8d591a9a6768cb8edd65d0
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-06-26 03:11:40 +0100

    creating post put delete and Get methods on the property controller

---
+++ backend/src/main/java/com/rent/backend/Controller/PropertyController.java
@@ -0,0 +1,38 @@
+package com.rent.backend.Controller;
+
+import com.rent.backend.DTO.PropertyDTO;
+import com.rent.backend.Model.Property;
+import com.rent.backend.Service.PropertyService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/property")
+public class PropertyController {
+    private final PropertyService service;
+
+    public PropertyController(PropertyService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/post")
+    public PropertyDTO addProperty(@RequestBody PropertyDTO dto){
+        return service.create(dto);
+    }
+
+    @PutMapping("/update/{id}")
+    public PropertyDTO updateProperty(@PathVariable Long id, @RequestBody PropertyDTO dto){
+        return service.update(id, dto);
+    }
+
+    @GetMapping("/get")
+    public List<PropertyDTO> getAll(){
+        return service.getAllProperties();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteProperty(@PathVariable Long id){
+        service.delete(id);
+    }
+}
