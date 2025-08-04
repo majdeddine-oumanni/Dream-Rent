198a486625f68ae280d4d15468fc037437437b05
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-08-04 17:59:18 +0100

    creating form for registration and styling it with bootstrap and css

--- frontend/src/app/Components/register/register.component.ts
+++ frontend/src/app/Components/register/register.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 import { UsersService } from '../../Service/users.service';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
 
+interface User {
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string,
+  role: string,
+  country: string
+};
 @Component({
   selector: 'app-register',
   imports: [FormsModule, CommonModule, ReactiveFormsModule],
@@ -12,23 +19,4 @@ import { Observable } from 'rxjs';
 })
 export class RegisterComponent {
   constructor(private service: UsersService){}
-user = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  password: '',
-  role: '',
-  country: ''
-};
-  onRegister(){
-    this.service.addUser(this.user).subscribe((response:any)=>{
-      console.log(response, this.user);
-    })
-  }
-  countries:any[]=[]
-  getCountries(){
-    return this.service.restoureCountries().subscribe((data:any)=>{
-      this.countries = data;
-    })
-  }
 }
