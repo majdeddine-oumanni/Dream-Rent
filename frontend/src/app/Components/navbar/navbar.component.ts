139608f14086acf8ad1a5c8e7ae548d3e7fe35b9
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-08-14 22:51:57 +0100

    creating the navbar component and implmenting the styling

--- frontend/src/app/Components/navbar/navbar.component.ts
+++ frontend/src/app/Components/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
 }
