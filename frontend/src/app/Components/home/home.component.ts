3096d435cc02e11331dd2d8627de2021b2049ca1
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-08-29 00:04:39 +0100

    implementing the api on propertie list for the home component

--- frontend/src/app/Components/home/home.component.ts
+++ frontend/src/app/Components/home/home.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from "@angular/router";
+import { PropertiesListService } from '../../Service/properties-list.service';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  available : boolean = true;
+export class HomeComponent implements OnInit{
+  constructor(private service : PropertiesListService) {}
+
+  properties : any = [];
+  ngOnInit(): void {
+      this.service.retrieveAllProperties().subscribe((data)=>{
+        this.properties = data;
+        console.log(this.properties);
+      })
+  }
+  createRange(count: number): number[] {
+    return Array.from({ length: Math.floor(count) }, (_, i) => i);
+  }
 }
--- frontend/src/app/Service/properties-list.service.ts
+++ frontend/src/app/Service/properties-list.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+
+interface Property{
+  title: string,
+  description: string,
+  country: string,
+  city: string,
+  roomsNumber: number,
+  area: number,
+  bathroomsNumber: number,
+  availability: boolean,
+  price: number,
+  guests: number,
+  //images: [],
+  propertyType: 'APARTMENT' | 'VILLA' | 'HOUSE',
+  avrgReview : number
+}
 @Injectable({
   providedIn: 'root'
 })
+
 export class PropertiesListService {
 
-  constructor() { }
+  baseUrl : string = "http://localhost:8080/api/properties";
+
+  constructor(private http: HttpClient) { }
+
+  retrieveAllProperties():Observable<Property[]>{
+    return this.http.get<Property[]>(this.baseUrl);
+  }
+
 }
