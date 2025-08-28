2ac394e233707cb85bed226c2264d056e795dbf1
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-08-28 02:38:53 +0100

    implementing a query to find the average review of a property

--- backend/src/main/java/com/rent/backend/Repositories/ReviewsRepository.java
+++ backend/src/main/java/com/rent/backend/Repositories/ReviewsRepository.java
@@ -2,9 +2,12 @@ package com.rent.backend.Repositories;
 
 import com.rent.backend.Model.Reviews;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ReviewsRepository extends JpaRepository<Reviews, Long> {
     List<Reviews> findAllByProperty_Id(Long id);
+    @Query(value = "SELECT AVG(r.review) FROM Reviews r WHERE r.property.id = :property_id")
+    float getAvgPropertyReviews(Long property_id);
 }
