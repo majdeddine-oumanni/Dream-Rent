e8408a93f916bb551936d8ea6bd5b0c8c2619c80
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-08-28 15:49:49 +0100

    modifyin routing

--- frontend/src/app/Components/navbar/navbar.component.ts
+++ frontend/src/app/Components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
