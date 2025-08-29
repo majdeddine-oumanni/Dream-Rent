912425d20010dde5dde818094d0d9bf3c7745e22
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-08-29 17:03:01 +0100

    solving the problem of null id with @Mapping(source = id, target = id)

--- backend/src/main/java/com/rent/backend/Mappers/PropertyMapper.java
+++ backend/src/main/java/com/rent/backend/Mappers/PropertyMapper.java
@@ -3,12 +3,15 @@ package com.rent.backend.Mappers;
 import com.rent.backend.DTO.PropertyDTO;
 import com.rent.backend.Model.Property;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PropertyMapper {
     Property toEntity(PropertyDTO dto);
+
+    @Mapping(source = "id", target = "id")
     PropertyDTO toDTO(Property property);
     List<PropertyDTO> toDTOs(List<Property> properties);
 }
