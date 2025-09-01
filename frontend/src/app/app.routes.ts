95045a8607d6070f033d78abc3f69427667e5bdb
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-09-01 23:36:09 +0100

    creating the profile component and installing the component package for bootstrap with npm i bootstrap-icons

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './Components/home/home.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { LoginComponent } from './Components/login/login.component';
 import { PropertyDetailsComponent } from './Components/property-details/property-details.component';
+import { ProfileComponent } from './Components/profile/profile.component';
 
 export const routes: Routes = [
   {path : "", component: HomeComponent},
   {path: "register", component: RegisterComponent},
   {path: "login", component: LoginComponent},
-  {path: "detail/:id", component: PropertyDetailsComponent}
+  {path: "detail/:id", component: PropertyDetailsComponent},
+  {path: "profile", component: ProfileComponent}
 ];
