b7ac54e074163ae50fc4437c32cb285b64203ffc
majd eddine oumanni <majdeddineoumanni@gmail.com>
2025-09-13 23:39:24 +0100

    creating the /property-update for updating property data

---
+++ frontend/src/app/Components/property-update/property-update.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-update',
+  imports: [],
+  templateUrl: './property-update.component.html',
+  styleUrl: './property-update.component.css'
+})
+export class PropertyUpdateComponent {
+
+}
